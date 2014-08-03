18b939badad2776eb092ecce90aa0dcf9ab40e02
Ross Nelson <ross.nelson@gmail.com>
2014-08-03 07:24:31 -0500

    s/omahelp/omafaq

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function omahelp() {
+function omafaq() {
   $('#search').hide();
   
   var content = $('#results');
