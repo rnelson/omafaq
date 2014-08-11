7efc93e55ea142122774f5efe386706d79ea7710
Ross Nelson <ross.nelson@gmail.com>
2014-08-11 07:29:05 -0500

    Added random response messages

--- js/js.js
+++ js/js.js
@@ -2,6 +2,19 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getResponseText() {
+  var messages = [
+    'We\'ve determined that you should call <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a> at <span class="results-number">402-444-5555</span>!',
+    'We searched the rolodex and believe this is the perfect answer: call <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a> at <span class="results-number">402-444-5555</span>!',
+    'We called your mom. She said the answer lies here, at <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a>. <span class="results-number">402-444-5555</span>',
+    'There is but one answer to this question: <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a> at <span class="results-number">402-444-5555</span>.',
+    'I wish I could say you were the first one to ask this question, but you are not special. Please call <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a> at <span class="results-number">402-444-5555</span>',
+    'You asked, the universe responded. Contact <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a> at <span class="results-number">402-444-5555</span>.'
+  ];
+  
+  return messages[getRandomInt(0, messages.length - 1)];
+}
+
 function omafaq() {
   $('#search').hide();
   
@@ -16,7 +29,7 @@ function omafaq() {
 }
 
 function showResults() {
-  data = '<img src="img/stothert-small.jpg"/><div class="results-text">We\'ve determined that you should call <a href="http://mayors-office.cityofomaha.org/index.php/mayor-s-hotline">the Mayor\'s Hotline</a> at <span class="results-number">402-444-5555</span>! <a href="#" onclick="return reset();">Search again</a></div>';
+  data = '<img src="img/stothert-small.jpg"/><div class="results-text">' + getResponseText() + ' <a href="#" onclick="return reset();">Search again</a></div>';
   
   var content = $('#results');
   content.empty();
